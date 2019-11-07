50776711f045f5bb368f48529bce5898c50b40cc
Kpatel114 <31674536+Kpatel114@users.noreply.github.com>
2019-11-06 20:30:47 -0600

    Project 2 2nd deliverable form page
    Created form page for project 2 website frayedknotarts.com

---
+++ contactform page/index.js
@@ -0,0 +1,5 @@
+document.getElementById("button").addEventListener("click", submitfunction);
+
+function submitFunction() {
+  alert("Thank you for the message! We will get back to you within 1-2 weeks");
+}
